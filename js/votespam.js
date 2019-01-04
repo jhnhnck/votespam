a8bea5327c14520a4c2791746c8e5d5afe41e0cf
John Hancock <john@dev.jhnhnck.com>
2019-01-04 21:24:14 +0000

    Fixed poll urls to use timestamps and added 200% more regex

--- js/votespam.js
+++ js/votespam.js
@@ -9,9 +9,10 @@
 
 import '/js/js-cookie.min.js'
 
-const voteURL = 'https://polldaddy.com/n/11aad1b680d8efba7827f20b7f5d778c/10203269?1546562786842'
-const urlVersion = 200
-const perSecond = 25
+const voteURL = 'https://polldaddy.com/n/11aad1b680d8efba7827f20b7f5d778c/10203269?'
+const altVoteURL = 'https://polls.polldaddy.com/vote-js.php?p=10203269&b=0&a=46892247,&o=&va=0&cookie=0&url=https%3A//www.flomarching.com/articles/6304511-tarp-vs-turf-comparing-indoor-percussion-to-outdoor&n='
+const urlVersion = 202
+const perSecond = 20
 const cookieConfig = { secure: true, expires: 28, domain: '.bcuzrobotics.com' }
 
 let cookie = {
@@ -27,10 +28,17 @@ document.getElementById('init-voter').addEventListener('click', VoterInit, false
 
 function VoterInit() {
   // Generate or merge browser cookie data
-  if (Cookies.get('__votespam') === undefined) {
+  const tmpCookie = Cookies.getJSON('__votespam')
+
+  if (tmpCookie === undefined) {
     Cookies.set('__votespam', cookie, cookieConfig)
+  } else if (tmpCookie.url < 202) {
+    Object.assign(cookie, tmpCookie)
+    cookie.contrib = 0
+    cookie.reported = -1
+    cookie.url = urlVersion
   } else {
-    Object.assign(cookie, Cookies.getJSON('__votespam'))
+    Object.assign(cookie, tmpCookie)
     cookie.url = urlVersion
   }
 
@@ -40,6 +48,8 @@ function VoterInit() {
   // Welome back message
   if (cookie.contrib > 0) {
     numDisplay.innerText = `Welcome back, ${cookie.contrib} votes already!`
+  } else if (cookie.reported === -1) {
+    numDisplay.innerText = 'Welcome back! New methods, so votes have been reset.'
   } else {
     numDisplay.innerText = `${cookie.contrib} votes`
   }
@@ -50,6 +60,11 @@ function VoterInit() {
   document.getElementById('init-voter').setAttribute('disabled', '')
   document.getElementById('cookie-warning').setAttribute('hidden', '')
   voter()
+
+  // Enable Percent field
+  setTimeout(() => {
+    document.getElementById('vote-percent-wrap').removeAttribute('hidden')
+  }, 5 * 1000)
 }
 
 function voter() {
@@ -71,12 +86,20 @@ function addVotes(votes) {
 
   // Shitty Reporting API
   if ((cookie.contrib - cookie.reported) > 1000) {
-    const http = new XMLHttpRequest()
+    const req = new XMLHttpRequest()
 
     // Prepare and send request
-    http.open('POST', '/api/votespam/report', true)
-    http.setRequestHeader('Content-type', 'application/json')
-    http.send(JSON.stringify(cookie));
+    req.open('POST', '/api/votespam/report', true)
+    req.setRequestHeader('Content-type', 'application/json')
+    req.send(JSON.stringify(cookie));
+
+    req.onreadystatechange = () => {
+      if (req.readyState === 4 && req.status === 200) {
+        if (req.response.currentURL > urlVersion) {
+          window.location.reload(true)
+        }
+      }
+    }
 
     // Update reported value
     cookie.reported = cookie.contrib
@@ -91,8 +114,16 @@ function uuidv4() {
 }
 
 function voteReq() {
-  return fetch(voteURL,
-    { mode: 'no-cors', cache: 'no-store', referrerPolicy: "no-referrer" }
-  )
+  const fetchOpts = { mode: 'no-cors', cache: 'no-store', referrerPolicy: "no-referrer" }
+
+  fetch(`${voteURL}${new Date().getTime()}`, fetchOpts).then((res) => {
+    const pdv = /[a-f0-9]+\|[0-9]+/.exec(res.text())[0]
+
+    fetch(`${altVoteURL}${pdv}`, fetchOpts).then((pdvres) => {
+      const percent = /<span\s[a-z="\-]+>\sClemson\s[^&]+&nbsp;([0-9.%]+)<\/span>/.exec(pdvres.text())[1]
+      document.getElementById('vote-percent').innerHTML = percent
+      return
+    })
+  })
 }
 
